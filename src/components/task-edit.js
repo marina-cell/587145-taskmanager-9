521fe65007e2d7d1d6f8a6c273536fa6c498fa7a
marina_cell <melissa_nsk_@mail.ru>
2019-09-27 13:39:10 +0700

    Добавляет карточку редактирования

---
+++ src/components/task-edit.js
@@ -0,0 +1,271 @@
+import {createElement} from '../utils.js';
+
+export default class TaskEdit {
+  constructor({description, dueDate, tags, color, repeatingDays}) {
+    this._description = description;
+    this._dueDate = new Date(dueDate);
+    this._tags = tags;
+    this._randomTags = Array.from(this._tags).sort(() => Math.random() - 0.5).slice(0, Math.round(Math.random() * 3));
+    this._color = color;
+    this._repeatingDays = repeatingDays;
+    this._isReapeting = Object.keys(this._repeatingDays).some((day) => this._repeatingDays[day]);
+    this._element = null;
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  getTemplate() {
+    return `<article class="card card--edit card--${this._color} ${this._isReapeting ? `card--repeat` : ``}">
+              <form class="card__form" method="get">
+                <div class="card__inner">
+                  <div class="card__control">
+                    <button type="button" class="card__btn card__btn--archive">
+                      archive
+                    </button>
+                    <button
+                      type="button"
+                      class="card__btn card__btn--favorites card__btn--disabled"
+                    >
+                      favorites
+                    </button>
+                  </div>
+
+                  <div class="card__color-bar">
+                    <svg class="card__color-bar-wave" width="100%" height="10">
+                      <use xlink:href="#wave"></use>
+                    </svg>
+                  </div>
+
+                  <div class="card__textarea-wrap">
+                    <label>
+                      <textarea
+                        class="card__text"
+                        placeholder="Start typing your text here..."
+                        name="text"
+                      >${this._description}</textarea>
+                    </label>
+                  </div>
+
+                  <div class="card__settings">
+                    <div class="card__details">
+                      <div class="card__dates">
+                        <button class="card__date-deadline-toggle" type="button">
+                          date: <span class="card__date-status">${this._dueDate ? `yes` : `no`}</span>
+                        </button>
+
+                        <fieldset class="card__date-deadline">
+                          <label class="card__input-deadline-wrap">
+                            <input
+                              class="card__date"
+                              type="text"
+                              placeholder=""
+                              name="date"
+                              value="${new Date(this._dueDate).toString()}"
+                            />
+                          </label>
+                        </fieldset>
+
+                        <button class="card__repeat-toggle" type="button">
+                          repeat:<span class="card__repeat-status">${this._isReapeting ? `yes` : `no`}</span>
+                        </button>
+
+                        <fieldset class="card__repeat-days">
+                          <div class="card__repeat-days-inner">
+                            <input
+                              class="visually-hidden card__repeat-day-input"
+                              type="checkbox"
+                              id="repeat-mo-4"
+                              name="repeat"
+                              value="mo"
+                              ${this._repeatingDays[`Mo`] ? `checked` : ``}
+                            />
+                            <label class="card__repeat-day" for="repeat-mo-4"
+                              >mo</label
+                            >
+                            <input
+                              class="visually-hidden card__repeat-day-input"
+                              type="checkbox"
+                              id="repeat-tu-4"
+                              name="repeat"
+                              value="tu"
+                              ${this._repeatingDays[`Tu`] ? `checked` : ``}
+                            />
+                            <label class="card__repeat-day" for="repeat-tu-4"
+                              >tu</label
+                            >
+                            <input
+                              class="visually-hidden card__repeat-day-input"
+                              type="checkbox"
+                              id="repeat-we-4"
+                              name="repeat"
+                              value="we"
+                              ${this._repeatingDays[`We`] ? `checked` : ``}
+                            />
+                            <label class="card__repeat-day" for="repeat-we-4"
+                              >we</label
+                            >
+                            <input
+                              class="visually-hidden card__repeat-day-input"
+                              type="checkbox"
+                              id="repeat-th-4"
+                              name="repeat"
+                              value="th"
+                              ${this._repeatingDays[`Th`] ? `checked` : ``}
+                            />
+                            <label class="card__repeat-day" for="repeat-th-4"
+                              >th</label
+                            >
+                            <input
+                              class="visually-hidden card__repeat-day-input"
+                              type="checkbox"
+                              id="repeat-fr-4"
+                              name="repeat"
+                              value="fr"
+                              ${this._repeatingDays[`Fr`] ? `checked` : ``}
+                            />
+                            <label class="card__repeat-day" for="repeat-fr-4"
+                              >fr</label
+                            >
+                            <input
+                              class="visually-hidden card__repeat-day-input"
+                              type="checkbox"
+                              name="repeat"
+                              value="sa"
+                              id="repeat-sa-4"
+                              ${this._repeatingDays[`Sa`] ? `checked` : ``}
+                            />
+                            <label class="card__repeat-day" for="repeat-sa-4"
+                              >sa</label
+                            >
+                            <input
+                              class="visually-hidden card__repeat-day-input"
+                              type="checkbox"
+                              id="repeat-su-4"
+                              name="repeat"
+                              value="su"
+                              ${this._repeatingDays[`Su`] ? `checked` : ``}
+                            />
+                            <label class="card__repeat-day" for="repeat-su-4"
+                              >su</label
+                            >
+                          </div>
+                        </fieldset>
+                      </div>
+
+                      <div class="card__hashtag">
+                        <div class="card__hashtag-list">
+
+                        ${this._randomTags.map((tag) => `
+                          <span class="card__hashtag-inner">
+                            <input
+                              type="hidden"
+                              name="hashtag"
+                              value="repeat"
+                              class="card__hashtag-hidden-input"
+                            />
+                            <p class="card__hashtag-name">
+                              #${tag}
+                            </p>
+                            <button type="button" class="card__hashtag-delete">
+                              delete
+                            </button>
+                          </span>`).join(``)}
+
+                        <label>
+                          <input
+                            type="text"
+                            class="card__hashtag-input"
+                            name="hashtag-input"
+                            placeholder="Type new hashtag here"
+                          />
+                        </label>
+                      </div>
+                    </div>
+
+                    <div class="card__colors-inner">
+                      <h3 class="card__colors-title">Color</h3>
+                      <div class="card__colors-wrap">
+                        <input
+                          type="radio"
+                          id="color-black-4"
+                          class="card__color-input card__color-input--black visually-hidden"
+                          name="color"
+                          value="black"
+                          ${this._color === `black` ? `checked` : ``}
+                        />
+                        <label
+                          for="color-black-4"
+                          class="card__color card__color--black"
+                          >black</label
+                        >
+                        <input
+                          type="radio"
+                          id="color-yellow-4"
+                          class="card__color-input card__color-input--yellow visually-hidden"
+                          name="color"
+                          value="yellow"
+                          ${this._color === `yellow` ? `checked` : ``}
+                        />
+                        <label
+                          for="color-yellow-4"
+                          class="card__color card__color--yellow"
+                          >yellow</label
+                        >
+                        <input
+                          type="radio"
+                          id="color-blue-4"
+                          class="card__color-input card__color-input--blue visually-hidden"
+                          name="color"
+                          value="blue"
+                          ${this._color === `blue` ? `checked` : ``}
+                        />
+                        <label
+                          for="color-blue-4"
+                          class="card__color card__color--blue"
+                          >blue</label
+                        >
+                        <input
+                          type="radio"
+                          id="color-green-4"
+                          class="card__color-input card__color-input--green visually-hidden"
+                          name="color"
+                          value="green"
+                          ${this._color === `green` ? `checked` : ``}
+                        />
+                        <label
+                          for="color-green-4"
+                          class="card__color card__color--green"
+                          >green</label
+                        >
+                        <input
+                          type="radio"
+                          id="color-pink-4"
+                          class="card__color-input card__color-input--pink visually-hidden"
+                          name="color"
+                          value="pink"
+                          ${this._color === `pink` ? `checked` : ``}
+                        />
+                        <label
+                          for="color-pink-4"
+                          class="card__color card__color--pink"
+                          >pink</label
+                        >
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="card__status-btns">
+                    <button class="card__save" type="submit">save</button>
+                    <button class="card__delete" type="button">delete</button>
+                  </div>
+                </div>
+              </form>
+            </article>`;
+  }
+}
