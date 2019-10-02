cff0b2155f47fe1477a4054ba0e8d5afb4bccc13
marina_cell <melissa_nsk_@mail.ru>
2019-10-02 10:25:24 +0700

    Статистика

--- src/components/statistics.js
+++ src/components/statistics.js
@@ -1,5 +1,7 @@
 import AbstractComponent from './components/abstract-component.js';
 
 export default class Statistics extends AbstractComponent {
-
+  const daysCtx = document.querySelector(`.statistic__days`);
+  const tagsCtx = document.querySelector(`.statistic__tags`);
+  const colorsCtx = document.querySelector(`.statistic__colors`);
 }
